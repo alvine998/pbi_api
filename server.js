c93feec1c473438a7fd274f1ad59820e45c0b1d3
alvine <alvinecom2018@gmail.com>
2026-01-17 00:32:53 +0700

    chore: Increase Express body parser payload limits to 50MB for JSON and URL-encoded requests.

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use("/uploads", express.static("uploads"));
 
 // Swagger Documentation
